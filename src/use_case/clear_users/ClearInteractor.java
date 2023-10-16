0602e5ae0cdb49fd5f203f53c60215457640ca60
Alice Sedgwick <alicejsedgwick2004@gmail.com>
2023-10-15 20:19:32 -0400

    updated methods

--- src/use_case/clear_users/ClearInteractor.java
+++ src/use_case/clear_users/ClearInteractor.java
@@ -2,5 +2,42 @@ package use_case.clear_users;
 
 // TODO Complete me
 
-public class ClearInteractor {
+import entity.User;
+import entity.UserFactory;
+import use_case.clear_users.ClearInputData;
+import use_case.clear_users.ClearOutputBoundary;
+import use_case.clear_users.ClearOutputData;
+import use_case.clear_users.ClearUserDataAccessInterface;
+
+import java.time.LocalDateTime;
+
+public class ClearInteractor implements ClearInputBoundary{
+    final ClearUserDataAccessInterface userDataAccessObject;
+    final ClearOutputBoundary userPresenter;
+    final UserFactory userFactory;
+
+    public SignupInteractor(SignupUserDataAccessInterface signupDataAccessInterface,
+                            SignupOutputBoundary signupOutputBoundary,
+                            UserFactory userFactory) {
+        this.userDataAccessObject = signupDataAccessInterface;
+        this.userPresenter = signupOutputBoundary;
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public void execute(SignupInputData signupInputData) {
+        if (userDataAccessObject.existsByName(signupInputData.getUsername())) {
+            userPresenter.prepareFailView("User already exists.");
+        } else if (!signupInputData.getPassword().equals(signupInputData.getRepeatPassword())) {
+            userPresenter.prepareFailView("Passwords don't match.");
+        } else {
+
+            LocalDateTime now = LocalDateTime.now();
+            User user = userFactory.create(signupInputData.getUsername(), signupInputData.getPassword(), now);
+            userDataAccessObject.save(user);
+
+            SignupOutputData signupOutputData = new SignupOutputData(user.getName(), now.toString(), false);
+            userPresenter.prepareSuccessView(signupOutputData);
+        }
+    }
 }
