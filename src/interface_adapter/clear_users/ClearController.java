88b2c79b33caf363e508ee87630561a85036d511
Alice Sedgwick <alicejsedgwick2004@gmail.com>
2023-10-15 20:17:49 -0400

    implemented ClearController

--- src/interface_adapter/clear_users/ClearController.java
+++ src/interface_adapter/clear_users/ClearController.java
@@ -1,5 +1,22 @@
 package interface_adapter.clear_users;
 
-// TODO Complete me
+import use_case.signup.SignupInputBoundary;
+import use_case.signup.SignupInputData;
+import use_case.clear_users.ClearInputData;
+import use_case.clear_users.ClearInputBoundary;
+
+// TODO Complete me seems good
 public class ClearController {
+
+    final ClearInputBoundary clearUseCaseInteractor;
+    public ClearController(ClearInputBoundary clearUseCaseInteractor) {
+        this.clearUseCaseInteractor = clearUseCaseInteractor;
+    }
+
+    public void execute(String username, String password) {
+        ClearInputData clearInputData = new ClearInputData(
+                username, password);
+
+        clearUseCaseInteractor.execute(clearInputData);
+    }
 }
